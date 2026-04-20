72c2de64c8b1fb4f12845331e9c7d9470254ae04
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-04-20 10:08:20 +0430

    new codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react"
+
 export default function App(){
+  const [value, setValue]=useState(10);
+  function hadleClick(){
+    setValue(value+1);
+  }
   return(
     <>
-    <h1></h1>
+    <div style={
+      {
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }
+    }>
+      <div style={
+        {
+            display: "flex",
+            gap:"10px",
+
+        }
+      }>
+      <h1>{value}</h1>
+      <button onClick={hadleClick} style={
+        {
+          
+        }
+      }>Add</button>
+      </div>
+    </div>
     </>
   )
 }
